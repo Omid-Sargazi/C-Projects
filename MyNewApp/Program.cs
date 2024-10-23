1a94d3b1f13d24ecf622304f3f4bce28dd2662d0
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-23 23:36:46 +0330

    Add two number with C#

---
+++ MyNewApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+        int num1=0;
+        string num = Console.ReadLine();
+        num1 = Convert.ToInt32(num);
+        num1++;
+        Console.WriteLine("" + num1);
+    }
+}
