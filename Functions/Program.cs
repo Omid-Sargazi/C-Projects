1a6f652bed2fe9b6fcd4dd6f72f1b4d045606705
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-24 16:59:32 +0330

    add two numbers and square them

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -5,10 +5,24 @@ class Program
     static void Main(string[] args)
     {
         PrintMessage();
+        int result = addTwoNumber(2,3);
+        Console.WriteLine("Add to number is: "+result);
+        Console.WriteLine("Square a number is: "+Square(10));
     }
 
     static void PrintMessage()
     {
         Console.WriteLine("Hello World");
+    }
+
+
+    static int addTwoNumber(int a, int b)
+    {
+        return a+b;
+    }
+
+    private static int Square(int a)
+    {
+        return a*a;
     }
 }
