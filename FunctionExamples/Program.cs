8ba06c5d62644ec2dd717b92398da0c58a542e67
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-24 23:00:20 +0330

    count a character in a string

--- FunctionExamples/Program.cs
+++ FunctionExamples/Program.cs
@@ -4,9 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter a number: ");
+    
+    Console.WriteLine("Enter a number: ");
     int number = int.Parse(Console.ReadLine());
     Console.WriteLine("Enter a number: "+ ReverseNumber(number));
+
+    Console.WriteLine("Enetr a string");
+    string s = Console.ReadLine();
+    int count=0;
+    Console.WriteLine("Enter the character to count:");
+    char character = Console.ReadLine()[0];
+    count = CountCharater(s,character);
+    Console.WriteLine("the count of the string is : "+count);
 }
    static int ReverseNumber(int num)
    {
@@ -16,5 +25,15 @@ class Program
         return helper(num/10, num%10+reverse*10);
     }
     return helper(num, 0);
+   }
+
+   static int CountCharater(string s, char C)
+   {
+    int count=0;
+    foreach(char c in s)
+    {
+        if(c==C) count++;
+    }
+    return count;
    }
 }
