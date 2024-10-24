38d71c7b3e0da4f6262d298b74c478977bacd70a
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-24 22:49:47 +0330

    reverse a number with recursive function

---
+++ FunctionExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.WriteLine("Enter a number: "+ ReverseNumber(number));
+}
+   static int ReverseNumber(int num)
+   {
+    static int helper(int num, int reverse)
+    {
+        if(num==0) return reverse;
+        return helper(num/10, num%10+reverse*10);
+    }
+    return helper(num, 0);
+   }
+}
