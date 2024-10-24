1fb04eb08d4baa89b4dda2e94a11d375403d648c
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-24 23:35:40 +0330

    Class in C#

---
+++ Classes/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace VehicleNamespace
+{
+    public class Car
+{
+    public string Model;
+    public int year;
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Car Model: {Model},Year: {year}");
+    }
+}
+
+public class Carr
+{
+    public string Model;
+    public int Year;
+
+    public Carr(string model, string year)
+    {
+        Model=model;
+        Year=year;
+    }
+
+    public void PrintDetails()
+    {
+        Console.WriteLine($"Car Model: {model}, Year: {year}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car  myCar = new Car();
+        myCar.Model = "Toyota";
+        myCar.year=2020;
+        myCar.PrintDetails();
+    }
+}
+
+}
---
+++ Functionexample02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+
+    static void PerfectSquare()
+    {
+        Console.WriteLine("Enetr a number: ");
+
+        int num = int.Parse(Console.ReadLine());
+
+        double sqrt 
+    }
+}
