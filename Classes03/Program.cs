4b667358825a90f247c1e96a4c6c87f3c95f9585
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-25 19:38:42 +0330

    Object Initializers

--- Classes03/Program.cs
+++ Classes03/Program.cs
@@ -9,6 +9,27 @@ namespace Excercise
             var product = new Product {Name="Laptop", Price=99.99M};
 
             Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            var products = new ProductEelctronic{ Name="TV", Price=89.36M};
+            Console.WriteLine($"Product: {products.Name} Price: {products.Price}");
+            Console.ResetColor();
+            var numbers = new List<int> {1,2,3,4,5,6,7,};
+            var sum = numbers.Sum();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Sum: {sum}");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("******************************");
+            Console.ResetColor();
+
+            var Person = new
+                        {
+                     Name = "John Doe",
+                    Age = 30,
+                    Address = new { Street = "123 Maple St", City = "Anytown" }
+                        };
+
+            Console.WriteLine($"Name: {Person.Name}, Age: {Person.Age}, Street: {Person.Address.Street}, City: {Person.Address.City} ");
         }
     }
 
@@ -16,6 +37,12 @@ namespace Excercise
     public class Product
     {
         public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductEelctronic
+    {
+        public string Name { get; set;}
         public decimal Price { get; set; }
     }
 }
