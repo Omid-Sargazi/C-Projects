9fde1b2fad685886279fa1e526b362952c7a44e9
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-25 19:03:54 +0330

    Automatic Properties

---
+++ Classes03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Excercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var product = new Product {Name="Laptop", Price=99.99M};
+
+            Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+        }
+    }
+
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
