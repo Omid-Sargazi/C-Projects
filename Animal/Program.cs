ba3b070ea020a4c69065869a3fa944090a15000d
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-27 21:12:01 +0330

    classes

---
+++ Animal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace  Class_Animal
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            Dog dog = new Dog();
+            dog.Eat();
+        }
+    }
+
+
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating.");
+        }
+    }
+
+    public class Dog:Animal
+    {
+    }
+}
+
