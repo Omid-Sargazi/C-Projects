4d6be61151e1ee254c6720648fe133eda1d88ea3
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-27 21:29:15 +0330

    inheritance

--- HelloWorld/TestApp/Program.cs
+++ HelloWorld/TestApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Classes_inheritance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Shape circle = new Circle(5);
+            Console.WriteLine(circle.Area());
+        }
+    }
+
+    public abstract class Shape{
+        public abstract double Area();
+    }
+
+    public class Circle:Shape{
+        public double Radius{get; set;}
+        public Circle(double radius) => Radius = radius;
+
+       public override double Area() => Math.PI * Radius * Radius;
+    }
+}
