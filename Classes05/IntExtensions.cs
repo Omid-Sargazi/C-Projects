45784181ed61bfc90a0efd8a71414516c055a984
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-27 20:28:54 +0330

    classes in c#

---
+++ Classes05/IntExtensions.cs
@@ -0,0 +1,10 @@
+namespace Classes
+{
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int number)
+        {
+            return number %2==0;
+        }
+    }
+}
---
+++ Classes05/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Classes{
+    public static class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            int num = 4;
+            bool isEven = num.IsEven();
+            Console.WriteLine($"{num} is even: {isEven}");
+        }
+    }
+}
