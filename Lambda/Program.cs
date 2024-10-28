26fa093ad50d549b58a51201071c1f5cf0f9ca3f
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-28 20:09:39 +0330

    Order By Descending

--- Lambda/Program.cs
+++ Lambda/Program.cs
@@ -8,7 +8,9 @@ public class Program
          int[] numbers={1,2,3,4,5,6,7,8,9,100,20,30,40,50,60,};
         var lambda1 = numbers.ToArray();
 
-        foreach (int item in lambda1)
+        var lambda2 = numbers.OrderByDescending(x=>x).ToArray();
+
+        foreach (int item in lambda2)
         {
             Console.WriteLine(item);
         }
