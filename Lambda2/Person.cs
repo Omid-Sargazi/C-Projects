757876678ae2c44ffdd4de92adbd8791cacff483
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-28 21:45:23 +0330

    Lambda exprestion

---
+++ Lambda2/Person.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Lambda
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Family { get; set; }
+
+        public int Age { get; set; }
+
+        public int PersonId { get; set; }
+    }
+}
---
+++ Lambda2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Lambda
+{
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Hello");
+                List<Person> people = new List<Person>();
+                Person p1 = new Person();
+                p1.Name="Reza";
+                p1.Family="Alishah";
+                p1.PersonId=1;
+                p1.Age=20;
+                people.Add(p1);
+
+
+                Person p2 = new Person(){
+                    PersonId=2,
+                    Name="Hadi",
+                    Family="Khatibi",
+                    Age=39,
+                };
+
+                people.Add(p2);
+
+                people.Add(new Person() {PersonId=3, Name="Abi", Family="daie", Age=21});
+
+                var result1 = people.ToList();
+
+                var result2 = people.OrderByDescending(p=>p.Age).ToList();
+
+                var result3 = people.Where(p=>p.Name.ToLower()=="reza").ToList();
+
+                var result4 = people.Select(p=>p.Name).ToList();
+
+                var result5 = people.Select(p=> new{
+                    p.Name, p.Age
+                }).ToList();
+
+
+                foreach(Person p in result3)
+                {
+                    Console.WriteLine($"{p.Name}:{p.Age} {p.Family} {p.PersonId}");
+                }
+                foreach(string p in result4)
+                {
+                    Console.WriteLine($"{p}");
+                }
+
+                foreach(var p in result5)
+                {
+                    Console.WriteLine($"{p.Name},{p.Age}");
+                }
+            }
+        }
+}
