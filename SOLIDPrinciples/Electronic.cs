786398c52c38d7d20934ed55aff7b818d3bb1ef6
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-29 22:27:18 +0330

    creating clasees and implements interfaces

---
+++ SOLIDPrinciples/Electronic.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyNamespace
+{
+    public class Electronics:IProducts, IDiscountable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal ApplyDiscount(decimal price) => price*0.9
+    }
+}
---
+++ SOLIDPrinciples/Grocery.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyNamespace
+{
+    public class Grocery:IProducts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
