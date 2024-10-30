7c819c64e12d33d6651d8cd2f1f89c83e8ab56ca
Omid-Sargazi <om.sargazi@gmail.com>
2024-10-31 00:00:13 +0330

    usage

--- SOLID02Principles/LibrarySystem/LibrarySyatem/Program.cs
+++ SOLID02Principles/LibrarySystem/LibrarySyatem/Program.cs
@@ -6,6 +6,10 @@ namespace LibrarySystem
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello");
+
+           Library library = new Library();
+            library.AddBook(new Book { Title = "1984", Author = "George Orwell", ISBN = "1234567890" });
+            library.ListBook();
         }
     }
 }
