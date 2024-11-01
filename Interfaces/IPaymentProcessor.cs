881ee32590095bf8c087ff3a1b4954e7173c0708
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-01 16:46:49 +0330

    Rigid Payment Processing System

--- Interfaces/IPaymentProcessor.cs
+++ Interfaces/IPaymentProcessor.cs
@@ -1,7 +1,17 @@
+using Interfaces;
+
 namespace Interfaces
 {
     public interface IPaymentProcessor
     {
         void ProcessPayment(decimal amount);
+    }
+}
+
+public class CreditCardProcessor:IPaymentProcessor
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing credit card payment of {amount}");
     }
 }
