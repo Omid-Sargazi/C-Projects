8bc3b9e89a80dcfd8eac28bc5b83c4c98b9725aa
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-03 19:01:41 +0330

    Linq01

---
+++ linq101/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Linq101
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            List<int> numbers = new List<int>{1,2,3,10,200,-200,20,37};
+            var evenNumber = numbers.Where(x=>x%2==0);
+            foreach(var x in evenNumber)
+            {
+                Console.WriteLine(x);
+            }
+            
+
+        }
+    }
+}
