bd7ecf9b45621a46ca93ff7588ec70f52c6d25a8
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-03 19:14:05 +0330

    linq 101 Length and where

--- linq101/Program.cs
+++ linq101/Program.cs
@@ -8,12 +8,25 @@ namespace Linq101
         {
             Console.WriteLine("Hello");
             List<int> numbers = new List<int>{1,2,3,10,200,-200,20,37};
-            var evenNumber = numbers.Where(x=>x%2==0);
+            var evenNumber = numbers.Where(x=>x%2==0).ToList();
             foreach(var x in evenNumber)
             {
                 Console.WriteLine(x);
             }
-            
+
+            for(int i=0;i<evenNumber.Count;i++)
+            {
+                Console.WriteLine(evenNumber[i]);
+            }
+            /////////////////////////////////////////
+            List<string> names = new List<string>{"Anna","Bob","Cahrlie","David"};
+            var shortNames = names.Where(x=>x.Length<=3).ToList();
+
+            foreach(var x in shortNames)
+            {
+                Console.WriteLine($"length less then 3 are: {x}");
+            }
+
 
         }
     }
