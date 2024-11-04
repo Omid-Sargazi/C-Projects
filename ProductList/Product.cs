6f1bb430032de7f489f76933af173c0925bcb91e
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-04 21:44:09 +0330

    define a Product class

---
+++ ProductList/Product.cs
@@ -0,0 +1,12 @@
+namespace IProduct
+{
+    public class Product
+    {
+        public int Id {get;set}
+        public string Name{get;set;}
+        public string Category{get;set;}
+        public double Price{get;set;}
+        public int StockQuantity{get;set;}
+        public DateTime CreatedDate{get;set;}
+    }
+}
---
+++ ProductList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IProduct
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+          var products = new List<Product>
+          {
+            new Product{Id=1,Name="Laptop",Category="Electronics",Price=1200,StockQuantity=10,CreatedDate=DateTime.Now.AddMonths(-2)},
+            new Product{Id=2,Name="SamrtPhone",Category="Electronics",Price=1500,StockQuantity = 15, CreatedDate = DateTime.Now.AddMonths(-1)}
+          }   
+        }
+    }
+
+}
