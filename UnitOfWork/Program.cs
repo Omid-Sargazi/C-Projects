af8d6b930f7e750c0a83932eea8417b8f13bb960
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-05 20:04:41 +0330

    Define the Entities without Unit of Work

---
+++ UnitOfWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UnitOfWOrk
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    public class Product
+    {
+        public int Id{get;set;}
+        public string Name{get;set;}
+        public int Stock{get;set;}
+    }
+
+    public class Order
+    {
+        public int Id{get;set;}
+        public int ProductId{get;set;}
+        public int Quantity{get;set;}
+        
+    }
+
+    public class Customer
+    {
+        public int Id{get;set;}
+        public string Name {get;set;}
+        public decimal Balance{get;set;}
+    }
+}
