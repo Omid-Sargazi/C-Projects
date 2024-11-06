496716858cc46c39aca965e73749985433c3f909
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-06 20:15:43 +0330

    Product entity

---
+++ CompleteUnitOfWork/Product.cs
@@ -0,0 +1,11 @@
+namespace UnitOfWorkComplete
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public List<Order> orders { get; set; }
+    }
+}
