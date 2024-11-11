8e227c3d2f307da5155e12a4f43831a1a0a4e693
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-11 21:56:52 +0330

    manage the repositories and save changes to the database

---
+++ LibraryManagementSystems/IUnitOfWork.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystems
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IRepository<Book> Books { get; }
+        IRepository<Author> Authors { get; }
+        IRepository<Member> Members { get; }
+        IRepository<Rental> Rentals { get; }
+        void Save();
+    }
+
+     public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LibraryContext _context;
+        public IRepository<Book> Books { get; private set; }
+        public IRepository<Author> Authors { get; private set; }
+        public IRepository<Member> Members { get; private set; }
+        public IRepository<Rental> Rentals { get; private set; }
+
+        public UnitOfWork(LibraryContext context)
+        {
+            _context = context;
+            Books = new Repository<Book>(_context);
+            Authors = new Repository<Author>(_context);
+            Members = new Repository<Member>(_context);
+            Rentals = new Repository<Rental>(_context);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
