aa65080663f5a6605dc8fd52ddab20a9c2d75ac5
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-11 20:23:29 +0330

    Encapsulation of Price

--- LibraryManagementSystem/Book.cs
+++ LibraryManagementSystem/Book.cs
@@ -10,5 +10,14 @@ namespace LibraryManagementSystem
         private decimal _price;
         public int AuthorId { get; set; }
         public Author Author { get; set; }
+
+        public void SetPrice(decimal price)
+        {
+            if(price<=0)
+            {
+                throw new ArgumentException("Price must be positive.");
+            }
+            _price = price;
+        }
     }
 }
