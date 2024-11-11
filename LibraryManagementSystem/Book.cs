85eca3b6debb7454b015b468ba26b2e6e67648f0
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-11 20:35:29 +0330

    mplementing IRentable in the Book Class

--- LibraryManagementSystem/Book.cs
+++ LibraryManagementSystem/Book.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace LibraryManagementSystem
 {
-    public class Book:BookBase
+    public class Book:BookBase,IRentable
     {
         private decimal _price;
         public int AuthorId { get; set; }
@@ -18,6 +18,21 @@ namespace LibraryManagementSystem
                 throw new ArgumentException("Price must be positive.");
             }
             _price = price;
+        }
+
+        public override decimal GetPrice()
+        {
+            return _price;
+        }
+
+        public Rental Rent(Member member)
+        {
+            var rental = new Rental{
+                Book=this,
+                Member=member,
+                RentalDate=DateTime.Now,
+            };
+            return rental;
         }
     }
 }
