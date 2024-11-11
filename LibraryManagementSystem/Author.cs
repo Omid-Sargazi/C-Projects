e8c85f52c6f516989d87783e974627dda528908b
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-11 20:18:11 +0330

    Member Class Inheriting from BaseEntity

--- LibraryManagementSystem/Author.cs
+++ LibraryManagementSystem/Author.cs
@@ -9,4 +9,10 @@ namespace LibraryManagementSystem
     {
         public List<Book> Books { get; set; } = new List<Book>();
     }
+
+    public class Member:BaseEntity
+    {
+        public string Email { get; set; }
+        public List<Rental> Rentals { get; set;} = new List<Rental>();
+    }
 }
