34a44c2044eef224c2e51c26c5c90bda9e585b41
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-12 20:22:06 +0330

    EcommerceContext

---
+++ ServiceProvider/EcommerceContext.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceProvider
+{
+    public class EcommerceContext:DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public EcommerceContext(DbContextOptions<EcommerceContext> options) : base(options)
+        {
+        }
+    }
+}
