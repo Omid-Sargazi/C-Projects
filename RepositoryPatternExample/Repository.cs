a0ef3f2c0913351f1d39771d3fe7dcf3f150c588
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-12 19:16:55 +0330

    Define the Repository Interfaces and Implementation

--- RepositoryPatternExample/Repository.cs
+++ RepositoryPatternExample/Repository.cs
@@ -5,8 +5,42 @@ using System.Threading.Tasks;
 
 namespace RepositoryPatternExample
 {
-    public class Repository
+    public class Repository:IRepository<T> where T:class
     {
-        
-    }
+        private readonly EcommerceContext _context;
+
+        public Repository(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public IEnumerable<T> Find(Func<T,bool> predicate)
+        {
+            return _context.Set<T>().where(predicate).ToList();
+        }
+
+ }
+
 }
