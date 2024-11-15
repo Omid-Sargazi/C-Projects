9d603e2fd45f8854c6ae206a717ccffeec3621e8
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-15 12:51:03 +0330

    all c# projects

---
+++ Cleanstructure/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cleanstructure
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
