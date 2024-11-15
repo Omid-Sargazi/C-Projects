267971b151374381561f34199dfb44ca94a603c4
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-15 14:03:43 +0330

    Commands & Command Handlers

--- Cleanstructure/Application/CreateBookCommand.cs
+++ Cleanstructure/Application/CreateBookCommand.cs
@@ -10,4 +10,24 @@ namespace Cleanstructure.Application
         public string Title { get; set; }
         public string ISBN { get; set; }
     }
+
+    public class CreateBookCommandHandler:IRequestHandler<CreateBookCommand,int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateBookCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+
+    public async Task<int> Handle(CreateBookCommand request,CancellationToken cancellationToken)
+    {
+        var book = new Book {Title=request.Title, ISBN=request.};
+        _unitOfWork.BookRepository
+        await _unitOfWork.CommitAsync();
+        return book.Id;
+
+    }
+    }
 }
