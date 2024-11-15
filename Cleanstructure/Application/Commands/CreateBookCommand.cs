5869b42cd74212d90e915d9699816e51f14f36f6
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-15 18:46:01 +0330

    Define the Command for Adding a Book

--- Cleanstructure/Application/Commands/CreateBookCommand.cs
+++ Cleanstructure/Application/Commands/CreateBookCommand.cs
@@ -3,31 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
+
 namespace Cleanstructure.Application.Commands
 {
     public class CreateBookCommand:IRequest<int>
     {
         public string Title { get; set; }
         public string ISBN { get; set; }
-    }
-
-    public class CreateBookCommandHandler:IRequestHandler<CreateBookCommand,int>
-    {
-        private readonly IUnitOfWork _unitOfWork;
-
-        public CreateBookCommandHandler(IUnitOfWork unitOfWork)
-    {
-        _unitOfWork = unitOfWork;
-    }
-
-
-    public async Task<int> Handle(CreateBookCommand request,CancellationToken cancellationToken)
-    {
-        var book = new Book {Title=request.Title, ISBN=request.};
-        _unitOfWork.BookRepository
-        await _unitOfWork.CommitAsync();
-        return book.Id;
-
-    }
     }
 }
--- Cleanstructure/Core/Entities/Book.cs
+++ Cleanstructure/Core/Entities/Book.cs
@@ -9,8 +9,6 @@ namespace Cleanstructure.Core.Entities
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string ISBN { get; set; }
-        public int AuthorId { get; set; }
-        public Author Author { get; set; }  
+        public string ISBN  { get; set; }
     }
 }
