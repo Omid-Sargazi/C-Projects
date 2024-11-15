15f285fe7a34020ba58b4aa98f46d77a2424cec1
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-15 14:15:39 +0330

    Repositories

--- Cleanstructure/Infrastructure/Repositories/IBookRepository.cs
+++ Cleanstructure/Infrastructure/Repositories/IBookRepository.cs
@@ -10,4 +10,24 @@ namespace Cleanstructure.Infrastructure.Repositories
         Task<List<Book>> GetAllBooksAsync();
         void Add(Book book);
     } 
+
+    public class BookRepository: IBookRepository
+    {
+        private readonly AppDbContext _context;
+    }
+
+    public BookRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Book>> GetAllBooksAsync()
+    {
+        return await _context.Books.Include(b=>b.Author).ToListAsync();
+    }
+
+    public void Add(Book book)
+    {
+        _context.Books.Add(book);
+    }
 }
