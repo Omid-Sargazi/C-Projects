4f6f54485dd448909bad26a23c4a2ae09ff19930
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-17 19:08:15 +0330

    Core Layer Book Entity

---
+++ BookManagementSystem02/Core/Entities/Book.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManagementSystem02.Core.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+
+        public Book(string title, string author, string isbn)
+        {
+            if(string.IsNullOrEmpty(title)) throw new ArgumentNullException("Title is required");
+            if(string.IsNullOrEmpty(author)) throw new ArgumentNullException("author is required");
+            if(string.IsNullOrWhiteSpace(isbn)) throw new ArgumentNullException("isbn is required");
+
+            Title=title;
+            Author=author;
+            ISbn=isbn;
+        }
+    }
+}
