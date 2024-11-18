0a146458265794cac88d398c2f611557ca84faa9
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-18 18:30:06 +0330

    Singelton pattern

---
+++ SingeltonPatterns/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SingeltonPatternn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            SingeltonPattern s1 = SingeltonPattern.GetInstance();
+            Console.WriteLine("Instance"+s1 );
+        }
+    }
+
+
+    public sealed class SingeltonPattern
+    {
+        private static  SingeltonPattern instance = null;
+
+        private SingeltonPattern(){}
+        public static SingeltonPattern GetInstance()
+        {
+            if(instance==null)
+            {
+                instance = new SingeltonPattern();
+            }
+            return instance;
+        }
+    }
+}
