c4eb6a2a47f5f6d1b01e40a8fb44843b0c34bd57
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-18 19:57:06 +0330

    ShapeFactory for creating products

---
+++ FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ShapeFactory
+    {
+        public IShape GetShape(string shapeType)
+        {
+            if(string.IsNullOrEmpty(shapeType)) return null;
+        }
+        switch (shapeType)
+        {
+            case "circle":
+                return new Circle();
+            case "rectangle":
+                return new Rectangle();
+            case "triangle":
+                return new Triangle();
+            
+            default:
+                throw new ArgumentException("Invalid Shape type");
+        }
+    }
+}
