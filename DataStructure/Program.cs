774f730119b80e1c2d84c4bee5bd8ef6b2df65e5
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-18 22:21:40 +0330

    Collections

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,6 +8,15 @@
         List<int> numbers = new List<int>{1,2,3};
         numbers.Add(100);
         Console.WriteLine(numbers.Count);
+
+
+        Console.WriteLine("////////////////////////////");
+        int[] arr = {1,2,3};
+        Stack<List<int>> stack = new Stack<List<int>>();
+        stack.Push(new List<int>(arr));
+        stack.Push(new List<int>(arr));
+        Console.WriteLine("Stack: "+stack.Count);
+        Console.WriteLine("Stack: "+stack.Pop());
         }
 
     }
