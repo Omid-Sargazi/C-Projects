0505880e4fb902f6b473ca8ff59b7ac850809755
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-18 18:50:20 +0330

    singelton another pattern

--- SingeltonPatterns/Program.cs
+++ SingeltonPatterns/Program.cs
@@ -9,6 +9,10 @@ namespace SingeltonPatternn
             Console.WriteLine("Hello");
             SingeltonPattern s1 = SingeltonPattern.GetInstance();
             Console.WriteLine("Instance"+s1 );
+            Console.WriteLine("///////////////////////////////////////////");
+
+            Logger log1 = Logger.GetInstance();
+            Console.WriteLine($"Instance of Log is: "+ log1);
         }
     }
 
@@ -25,6 +29,31 @@ namespace SingeltonPatternn
                 instance = new SingeltonPattern();
             }
             return instance;
+        }
+    }
+
+
+    public sealed class Logger
+    {
+        private static Logger _instance;
+
+        private Logger()
+        {
+            Console.WriteLine("Logger Initialized");
+        }
+
+        public static Logger GetInstance()
+        {
+            if(_instance==null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log:{message}");
         }
     }
 }
