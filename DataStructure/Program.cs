5581b01c5a551ea1374d2df194de3b0515667230
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-18 21:25:16 +0330

    array

---
+++ DataStructure/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructure
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        List<int> numbers = new List<int>{1,2,3};
+        numbers.Add(100);
+        Console.WriteLine(numbers.Count);
+        }
+
+    }
+}
