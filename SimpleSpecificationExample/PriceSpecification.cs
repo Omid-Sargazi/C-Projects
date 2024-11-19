05ad05ca1fac155dca87ffcc87ad851c31a3b6a4
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-19 20:22:58 +0330

    PriceSpecification

---
+++ SimpleSpecificationExample/PriceSpecification.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleSpecificationExample
+{
+    public class PriceSpecification:ISpecification<Product>
+    {
+        private readonly decimal _minPrice;
+
+        public PriceSpecification(decimal minPrice)
+        {
+            _minPrice = minPrice;
+        }
+
+        public Expression<Func<Product,bool>> Criteria => product=>product.Price>=_minPrice;
+
+        public bool IsSatisfiedBy(Product product)
+        {
+            return product.Price>=_minPrice;
+        }
+    }
+}
