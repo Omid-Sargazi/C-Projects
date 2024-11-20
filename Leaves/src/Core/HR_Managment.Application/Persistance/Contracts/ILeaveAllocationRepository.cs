76b9d5e1972492276b4fe5245e4b954459e41c4c
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-20 23:28:55 +0330

    using automapper and add class library into solution

--- Leaves/src/Core/HR_Managment.Application/Persistance/Contracts/ILeaveAllocationRepository.cs
+++ Leaves/src/Core/HR_Managment.Application/Persistance/Contracts/ILeaveAllocationRepository.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HR_Managment.Domin;
 
 namespace HR_Managment.Application.Persistance.Contracts
 {
-    public interface ILeaveAllocationRepository:IGenericRepository<LeaLeaveAllocation>
+    public interface ILeaveAllocationRepository:IGenericRepository<LeaveAllocation>
     {
         
     }
--- Leaves/src/Core/HR_Managment.Application/Persistance/Contracts/ILeaveTypeRepository.cs
+++ Leaves/src/Core/HR_Managment.Application/Persistance/Contracts/ILeaveTypeRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HR_Managment.Domin;
 
 namespace HR_Managment.Application.Persistance.Contracts
 {
