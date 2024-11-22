81c34be317749bf4ab7de54713405405f2b8cc19
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-22 18:33:49 +0330

    event

---
+++ EventInCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace EventInCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+
+            var button = new Button();
+            button.Clicked +=OnButtonClicked;
+            button.Click();
+
+        }
+
+        private static void OnButtonClicked()
+        {
+            Console.WriteLine("Button Clicked Event Handled!");
+        }
+
+
+    }
+
+    public class Button
+    {
+        public event Action Clicked;
+
+        public void Click()
+        {
+            Console.WriteLine("Button clicked!");
+            Clicked?.Invoke();
+        }
+    }
+}
