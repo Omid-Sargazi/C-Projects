1ce8963422b681d971794a5fedbad0f93c33f120
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-22 17:12:36 +0330

    create employee calss

---
+++ UnifiedType/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UnifiedType
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 41;
+            var employee = 
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Employee: {Name}, Age{Age}";
+        }
+    }
+}
