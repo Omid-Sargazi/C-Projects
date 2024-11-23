8c22c01514bc94a388e41b948ca8e027b3d885d1
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-23 18:33:20 +0330

    define anonymous delegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -3,9 +3,19 @@
     public class Program
     {
         delegate int Deleg1(int a, int b);  
+        delegate void Deleg3();
         public static void Main(string[] args)
         {
-           Deleg1 deleg1 = delegate(int a, int b){ return a + b; };
+           Deleg1 deleg1 = delegate(int a, int b){ 
+            Console.WriteLine("Hello");
+            return a + b; };
+           int result = deleg1(100,20);
+           Console.WriteLine(result);
+
+           Deleg1 deleg2 = (x,y)=>x+y;
+           int result2 = deleg2(1000,200);
+           Console.WriteLine(result2);
+
         }
 
        
