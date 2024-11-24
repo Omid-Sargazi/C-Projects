640a3026575e8dd1ad59625586b2f03abe82f117
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-24 20:14:22 +0330

    elementry C#

---
+++ W3SchoolCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace W3SchoolCSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello");
+
+            long largeNumber = 9223372L;
+            int num1 = Convert.ToInt32(largeNumber);
+            Console.WriteLine("int32"+largeNumber);
+            Console.WriteLine(num1);
+            string num2 = Convert.ToString(num1);
+            Console.WriteLine("string num2: "+num2);
+            bool num3 = Convert.ToBoolean(num1);
+
+            Console.WriteLine("Boolean: "+num3);
+            Console.WriteLine(largeNumber);
+
+
+
+        }
+    }
+}
