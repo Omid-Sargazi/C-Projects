bec08c8fee815a5500df8aa0609889f83ca55bce
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-25 16:51:52 +0330

    Concatenation and Interpolation

--- W3SchoolCSharp/Program.cs
+++ W3SchoolCSharp/Program.cs
@@ -5,19 +5,24 @@ namespace W3SchoolCSharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            // Console.WriteLine("hello");
 
-            long largeNumber = 9223372L;
-            int num1 = Convert.ToInt32(largeNumber);
-            Console.WriteLine("int32"+largeNumber);
-            Console.WriteLine(num1);
-            string num2 = Convert.ToString(num1);
-            Console.WriteLine("string num2: "+num2);
-            bool num3 = Convert.ToBoolean(num1);
+            // long largeNumber = 9223372L;
+            // int num1 = Convert.ToInt32(largeNumber);
+            // Console.WriteLine("int32"+largeNumber);
+            // Console.WriteLine(num1);
+            // string num2 = Convert.ToString(num1);
+            // Console.WriteLine("string num2: "+num2);
+            // bool num3 = Convert.ToBoolean(num1);
 
-            Console.WriteLine("Boolean: "+num3);
-            Console.WriteLine(largeNumber);
+            // Console.WriteLine("Boolean: "+num3);
+            // Console.WriteLine(largeNumber);
+            string s1="omid";
+            string s2="sargazi";
 
+            Console.WriteLine(string.Concat(s1, s2));
+
+            Console.WriteLine($"My full name is: {s1} {s2}");
 
 
         }
