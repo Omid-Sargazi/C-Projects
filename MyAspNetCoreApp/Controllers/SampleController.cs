0b76095c8ba30508b0df0df3d25ad2c8d711eb16
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-27 23:19:20 +0330

    update Controllers

--- MyAspNetCoreApp/Controllers/SampleController.cs
+++ MyAspNetCoreApp/Controllers/SampleController.cs
@@ -1,70 +1,45 @@
-using Microsoft.AspNetCore.Mvc;
-using FluentValidation;
-using FluentValidation.Results;
-using System;
-using MyAspNetCoreApp.Models;
-
-// [ApiController]
-// [Route("api/custom-route")]
-// public class SampleController : ControllerBase
-// {
-//     private readonly IValidator<Pesron> _validator;
-
-//     public SampleController(IValidator<Pesron> validator)
-//     {
-//         _validator = validator;
-//     }
-
-
-//     [HttpGet]
-//     public IActionResult ValidatePerson([FromBody] Pesron person)
-//     {
-        
-//          ValidationResult result = _validator.Validate(person);
-
-//         if (!result.IsValid)
-//         {
-//             return BadRequest(result.Errors);
-//         }
-
-//         return Ok("Person is valid!");
-//     }
-// }
-
-// SampleController.cs
 using MediatR;
-// using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using MyAspNetCoreApp.Requests;
 
 [Route("api/[controller]")]
 [ApiController]
-public class SampleController : ControllerBase
+public class PersonController : ControllerBase
 {
     private readonly IMediator _mediator;
 
-    public SampleController(IMediator mediator)
+    public PersonController(IMediator mediator)
     {
         _mediator = mediator;
     }
 
-    [HttpPost("create-person")]
+    [HttpPost("create")]
     public async Task<IActionResult> CreatePerson([FromBody] CreatePersonCommand02 command)
     {
-        var result = await _mediator.Send(command);  // Send the command using MediatR
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPerson(int id)
+    {
+        var query = new GetPersonQuery { Id = id };
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdatePerson([FromBody] UpdatePersonCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeletePerson(int id)
+    {
+        var command = new DeletePersonCommand { Id = id };
+        var result = await _mediator.Send(command);
         return Ok(result);
     }
 }
-
-
-
-// [ApiController]
-// [Route("api/custom-route")]
-// public class SampleController : ControllerBase
-// {
-//     [HttpPost]
-//     public IActionResult ValidatePerson([FromBody] Pesron person)
-//     {
-//         // Validation logic here
-//         return Ok("Person is valid!");
-//     }
-// }
