2c31fee29b4b44ecde5ac50ac59e6e868797fd0e
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-30 17:33:19 +0330

    usage

---
+++ DIExamples/Program.cs
@@ -0,0 +1,14 @@
+﻿using DIExamples.DI;
+
+namespace DIExamples
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IMessageService emailService = new EmailService();
+            Notification notification= new Notification(emailService);
+            notification.Notify("Hello, Dependency Injection!");
+        }
+    }
+}
