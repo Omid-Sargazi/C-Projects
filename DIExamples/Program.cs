2f353e4909b094bfe2a5ab0774cc610eba7ce0de
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-30 18:58:42 +0330

    usage with AddTransient method

--- DIExamples/Program.cs
+++ DIExamples/Program.cs
@@ -1,4 +1,4 @@
-﻿using DIExamples.DI;
+﻿using DIExamples.Dependency;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DIExamples
@@ -14,12 +14,28 @@ namespace DIExamples
             // notification1.Notify("Hello via SMS!");
             // notification.Notify("Hello, Dependency Injection!");
 
-            var serviceProvider = new ServiceCollection()
-            .AddTransient<IMessageService,EmailService>()
-            .BuildServiceProvider();
+            // var serviceProvider = new ServiceCollection()
+            // .AddTransient<IMessageService,EmailService>()
+            // .BuildServiceProvider();
 
-            var notification = new Notification(serviceProvider.GetService<IMessageService>());
-            notification.Notify("Hello using DI Container");
+            // var notification = new Notification(serviceProvider.GetService<IMessageService>());
+            // notification.Notify("Hello using DI Container");
+
+        //     IMessageService02 emailService = new EmailService02();
+        //    Notification notification = new Notification(emailService);
+        //    notification.Notify("hello");
+        //    IMessageService02 smsService = new SmsService();
+        //    Notification notification1 = new Notification(smsService);
+        //    notification.Notify("sms service");
+
+        var serviceProvider = new ServiceCollection()
+        .AddTransient<IMessageService02,EmailService02>()
+        .BuildServiceProvider();
+
+        var notification = new Notification(serviceProvider.GetService<IMessageService02>());
+        notification.Notify("Dependancy injection with packages");
+
+
         }
     }
 }
