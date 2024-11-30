de17fd1013aaad1276368e96fa886da2e20e1dde
Omid-Sargazi <om.sargazi@gmail.com>
2024-11-30 19:42:09 +0330

    create interface

---
+++ DIWebServer/Interfaces/IGreeterService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIWebServer.Interfaces
+{
+    public interface IGreeterService
+    {
+        string Greet(string name);
+    }
+}
