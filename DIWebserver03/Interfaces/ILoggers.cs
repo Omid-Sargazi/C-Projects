5a0e6a535e2b1c50b18748b8c76fe21db1625e0e
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-01 19:16:55 +0330

    interfaces for DI constructore

---
+++ DIWebserver03/Interfaces/ILoggers.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIWebserver03.Interfaces
+{
+    public interface ILoggers
+    {
+        void Log(string message);
+    }
+}
