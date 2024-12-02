bf7832e5b03432e08f24b94ee15b5d6b623d7a67
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-02 19:42:10 +0330

    FluentValidation Validators for creating

---
+++ LibrarySystemBackend/Validators/BookCreateDtoValidator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using LibrarySystemBackend.DTOs;
+
+namespace LibrarySystemBackend.Validators
+{
+    public class BookCreateDtoValidator:AbstractValidator<BookCreateDto>
+    {
+        public BookCreateDtoValidator()
+        {
+            RuleFor(b=>b.Title).NotEmpty().WithMessage("Title is required.");
+            RuleFor(b=>b.PublishedDate).LessThanOrEqualTo(DateTime.Now).WithMessage("Published date cannot be in the future.");
+        }
+    }
+}
