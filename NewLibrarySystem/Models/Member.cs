9afa58c50c9176ecadbdb7bfeec5a9f2e7673b7a
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-03 23:01:04 +0330

    create a new model and define its relationship

--- NewLibrarySystem/Models/Member.cs
+++ NewLibrarySystem/Models/Member.cs
@@ -7,6 +7,9 @@ namespace NewLibrarySystem.Models
 {
     public class Member
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<BorrowRecord> BorrowRecords{ get; set; }
     }
 }
