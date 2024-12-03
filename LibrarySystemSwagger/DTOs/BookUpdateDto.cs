c086b7d4ab93135fb2505f5a5d4323202a192b8c
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-03 17:24:23 +0330

    create model and DTOs

---
+++ LibrarySystemSwagger/DTOs/BookUpdateDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystemSwagger.DTOs
+{
+    public class BookUpdateDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ LibrarySystemSwagger/Models/Book.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibrarySystemSwagger.Models
+{
+    public class Book
+    {
+        public int Id { get; set;}
+        public string Title { get; set;}
+        public DateTime PublishedDate   { get; set;}
+        public bool IsAvailable { get; set;}
+    }
+}
