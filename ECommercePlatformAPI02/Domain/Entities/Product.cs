81ae8f86d4c883aaa1c503ebbc638335cca6f151
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-06 16:57:28 +0330

    create model and abstarct class

---
+++ ECommercePlatformAPI02/Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price {get;set;}
+        public int Stock {get;set;} 
+    }
+}
