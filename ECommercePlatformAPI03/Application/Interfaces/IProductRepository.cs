66cd9b07285fa157620cd567fe303e4af2d5d10f
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-07 22:23:40 +0330

    Create an Interface for Database Access

---
+++ ECommercePlatformAPI03/Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> AddProductAsync(Product product,CancellationToken cancellationToken);
+        Task<List<Product>> GetAllProductsAsync(CancellationToken cancellationToken);
+    }
+}
