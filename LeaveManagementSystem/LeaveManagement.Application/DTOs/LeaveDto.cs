f18c72edd896731fc48a519b81f09d76bc46ed00
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-10 18:40:33 +0330

    Create DTOs

---
+++ LeaveManagementSystem/LeaveManagement.Application/DTOs/LeaveDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Application.DTOs
+{
+    public class LeaveDto
+    {
+        public string EmployeeId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+    }
+}
