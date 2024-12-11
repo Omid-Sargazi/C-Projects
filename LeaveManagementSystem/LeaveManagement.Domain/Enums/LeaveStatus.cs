89ffced13540c85b865421a8c17f1538db4dc064
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-11 19:07:30 +0330

    define status of the entity

--- LeaveManagementSystem/LeaveManagement.Domain/Enums/LeaveStatus.cs
+++ LeaveManagementSystem/LeaveManagement.Domain/Enums/LeaveStatus.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace LeaveManagement.Domain.Enums
 {
     public enum LeaveStatus
     {
-        Pending,
+         Pending,
         Approved,
-        Rejected,
+        Rejected
     }
 }
