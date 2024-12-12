a6d39b0cf59f88f0776e800902307f1510fad360
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-12 12:50:23 +0330

    Add Relationships Between Entities

--- LeaveManagementSystem/LeaveManagement.Domain/Entities/Employee.cs
+++ LeaveManagementSystem/LeaveManagement.Domain/Entities/Employee.cs
@@ -10,7 +10,9 @@ namespace LeaveManagement.Domain.Entities
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string DepartmentId { get; set; }
+        public int DepartmentId { get; set; }
         public string Email { get; set; }
+        public Department Department { get; set; }
+        public ICollection<Leave> Leaves { get; set; }
     }
 }
--- LeaveManagementSystem/LeaveManagement.Domain/Entities/Leave.cs
+++ LeaveManagementSystem/LeaveManagement.Domain/Entities/Leave.cs
@@ -8,10 +8,11 @@ namespace LeaveManagement.Domain.Entities
     public class Leave
     {
         public int Id { get; set; }
-        public string? EmployeeId { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string? Reason { get; set; }
         public string? Status { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
     }
 }
