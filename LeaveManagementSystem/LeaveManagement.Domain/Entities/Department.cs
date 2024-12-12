ad94e0a7844616304d9669db353bc662fe15d657
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-12 12:46:22 +0330

    Add Relationships Between Entities and Update Domain Entities

--- LeaveManagementSystem/LeaveManagement.Domain/Entities/Department.cs
+++ LeaveManagementSystem/LeaveManagement.Domain/Entities/Department.cs
@@ -10,5 +10,6 @@ namespace LeaveManagement.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public ICollection<Employee> Employees { get; set; }
     }
 }
