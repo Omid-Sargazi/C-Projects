56fb707f520568e6e61d44104081e2afaf3bab42
Omid-Sargazi <om.sargazi@gmail.com>
2024-12-13 18:38:22 +0330

    define a event entity

---
+++ EventManagementSystem/EventManagementSystem.Domain/Entities/Event.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventManagementSystem.Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int VenueId { get; set; }
+        public int OrganizerId { get; set; }
+        public List<int> AttendeeIds { get; set; }=new();
+    }
+}
